3f9f201f31519d254dae97f612da1ae1e6389dc3
hanyongbin <m15558038069@163.com>
2016-08-31 13:52:37 +0800

    char.js完善

--- char.js
+++ char.js
@@ -1,30 +1,31 @@
 /**
  * Created by hyb on 16/8/31.
  */
-var net=require('net');
-var ChartServer=net.createServer();
-var clientList=[];
-ChartServer.on("connection",function(client){
+/**code*/
+var net = require('net');   //加载net模块
+var chartServer=net.createServer();  //创建一个服务
+var clientList=[]; //建立一个客户端数组
+chartServer.on('connection',function(client){  //监听服务器端连接
     client.name=client.remoteAddress+':'+client.remotePort;
+    client.write("Welcome to hyb's server");  //像连接的客户端输出信息
     console.log(client.name+" joined!");//通知服务器谁来了
-    clientList.push(client);
-    client.write("welcome to hyb's server...");
-    client.on("data",function(data){
-        for(var i = 0;i < clientList.length;i++){
-            if(client!=clientList[i]){
+    clientList.push(client);  //把连接的客户端推入数组
+    client.on('data',function(data){  //监听客户端输入
+        for(var i=0;i<clientList.length;i++){
+            if(client != clientList[i]){  //当不是自身客户端时，像别的已连接的客户端输出信息
                 clientList[i].write(client.name+":"+data.toString());
             }
         }
         console.log(client.name+":"+data.toString());
     });
     client.on("end",function(){
-        clientList.splice(clientList.indexOf(client),1);
+        clientList.splice(clientList.indexOf(client),1); //从数组里删除离开的客户端
         console.log(client.name+" left!"); //通知服务器谁离开了
     });
     client.on("error",function(e){
-       console.log(e);
+        console.log(e);
     });
-
 });
 
-ChartServer.listen(9000);
+
+chartServer.listen(9000);  //监听9000端口
