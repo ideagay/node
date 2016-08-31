5648a1c6d2e30085bc14fcda3d813983c0ab7c4c
hanyongbin <m15558038069@163.com>
2016-08-31 13:44:55 +0800

    char.js完善

--- char.js
+++ char.js
@@ -2,19 +2,29 @@
  * Created by hyb on 16/8/31.
  */
 var net=require('net');
-var chartServer=net.createServer();
+var ChartServer=net.createServer();
 var clientList=[];
-chartServer.on("connection",function(client){
+ChartServer.on("connection",function(client){
+    client.name=client.remoteAddress+':'+client.remotePort;
+    console.log(client.name+" joined!");//通知服务器谁来了
     clientList.push(client);
     client.write("welcome to hyb's server...");
     client.on("data",function(data){
         for(var i = 0;i < clientList.length;i++){
             if(client!=clientList[i]){
-                clientList[i].write(data.toString())
+                clientList[i].write(client.name+":"+data.toString());
             }
         }
-        console.log(data.toString());
-    })
+        console.log(client.name+":"+data.toString());
+    });
+    client.on("end",function(){
+        clientList.splice(clientList.indexOf(client),1);
+        console.log(client.name+" left!"); //通知服务器谁离开了
+    });
+    client.on("error",function(e){
+       console.log(e);
+    });
+
 });
 
-chartServer.listen(9000);
+ChartServer.listen(9000);
